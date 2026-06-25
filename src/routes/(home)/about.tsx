7fb22a0e5a3f14f901a57d996491794746f6ed4a
MejanH <miza9ul@gmail.com>
2026-06-25 16:27:40 +0600

    chore

--- src/routes/(home)/about.tsx
+++ src/routes/(home)/about.tsx
@@ -5,5 +5,8 @@ export const Route = createFileRoute('/(home)/about')({
 })
 
 function AboutPage() {
-  return <h1>About Page, version 0.0.4</h1>
+  return <div>
+    <h1>About</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium quos magni laboriosam officia, iste nam ipsa quas dolore repellat impedit, sed minima perspiciatis officiis sint? Provident voluptatem tempora nam laudantium!</p>
+  </div>
 }
